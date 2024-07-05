665aeb233dc98b116fc32ce3afd7afd398f1b37a
Stefan Arsic <stefanarsic1201@gmail.com>
2024-07-05 17:12:22 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={2} /> */}
+    <App />
+  </React.StrictMode>
+);
